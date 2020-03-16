ef1b3765930c9264a3d65ae951f46b808b2b49a4
Adrian Szlegel <adrian@oaknode.com>
2020-03-16 11:46:27 +0100

    Navigation and welcome section added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React from 'react';
+import Header from './components/Header'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Adrian Szlegel - React based portfolio website
-        </p>
+    <React.Fragment>
+      <header>
+        <Header />
       </header>
-    </div>
+      <div id="welcome-section">
+        <h1 id="name" className="text-center">Adrian Szlegel</h1>
+        <p className="text-center">Full Stack Developer</p>
+        <p className="text-center" ><a href="https://adrszl.github.io/">Static version</a></p>
+        <div className="text-center">
+          <img src={logo} className="App-logo" alt="react" />
+        </div>
+      </div>
+    </React.Fragment>
   );
 }
 
