d8ade7653a30bf615f665525653b7eba15c75807
Adrian Szlegel <adrian@oaknode.com>
2020-04-18 12:46:58 +0200

    Visual changes to ExperienceCards

--- src/components/ExperienceCard.js
+++ src/components/ExperienceCard.js
@@ -1,31 +1,44 @@
 import React from 'react';
+import './ExperienceCard.css';
 
 function ExperienceCard(props) {
     let classes = 'card col-sm-5 col-md-4 col-lg-3 col-xl-2 offset-sm-2';
     switch(props.order) {
         case 1:
-            classes += ' offset-lg-2 offset-xl-2';
+            classes += ' offset-md-2 offset-lg-2 offset-xl-2';
+            break;
+        case 2:
+            classes += ' offset-md-0 offset-lg-0 offset-xl-0';
             break;
         case 3:
-            classes += ' offset-md-2';
+            classes += ' offset-md-2 offset-lg-0 offset-xl-0';
             break;
         case 4:
-            classes += ' offset-lg-2';
+            classes += ' offset-md-0 offset-lg-2 offset-xl-0';
             break;
         case 5:
-            classes += ' offset-xl-2';
+            classes += ' offset-md-2 offset-lg-0 offset-xl-2';
+            break;
+        case 6:
+            classes += ' offset-md-0 offset-lg-0 offset-xl-0';
             break;
         case 7:
-            classes += ' offset-md-2 offset-lg-2';
+            classes += ' offset-md-2 offset-lg-2 offset-xl-0';
+            break;
+        case 8:
+            classes += ' offset-md-0 offset-lg-0 offset-xl-0';
             break;
         case 9:
-            classes += ' offset-xl-2';
+            classes += ' offset-md-2 offset-lg-0 offset-xl-2';
             break;
         case 10:
-            classes += ' col-xl-2 offset-lg-2';
+            classes += ' offset-md-0 offset-lg-2 offset-xl-0 col-xl-2';
             break;
         case 11:
-            classes += ' offset-md-2';
+            classes += ' offset-md-2 offset-lg-0 col-xl-0';
+            break;
+        case 12:
+            classes += ' offset-md-0 offset-lg-0 offset-xl-0';
             break;
         default:
             break;
@@ -35,7 +48,7 @@ function ExperienceCard(props) {
         <div className={classes}>
             <div className="card-body">
             <h5 className="card-title">{props.title}</h5>
-            <h6 className="card-subtitle mb-2 text-muted">{props.year}</h6>
+            <h6 className="card-subtitle mb-2 text-muted"><time>{props.year}</time></h6>
             <p className="card-text">{props.details}</p>
             {props.link ? 
                 <a href={props.link} className="card-link" target="_blank">{linkLabel}</a>
