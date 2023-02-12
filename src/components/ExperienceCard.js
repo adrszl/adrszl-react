57bc8ac2a240d0f00cd44e2c7f62f2ceb13ca964
Adrian Szlegel <adrian.szlegel@hotmail.com>
2023-02-12 20:57:38 +0100

    display grid changes and adjustments & new experience card added

--- src/components/ExperienceCard.js
+++ src/components/ExperienceCard.js
@@ -2,50 +2,9 @@ import React from 'react';
 import './ExperienceCard.css';
 
 function ExperienceCard(props) {
-    let classes = 'card col-sm-5 col-md-4 col-lg-3 col-xl-2 offset-sm-2';
-    switch(props.order) {
-        case 1:
-            classes += ' offset-md-2 offset-lg-2 offset-xl-2';
-            break;
-        case 2:
-            classes += ' offset-md-0 offset-lg-0 offset-xl-0';
-            break;
-        case 3:
-            classes += ' offset-md-2 offset-lg-0 offset-xl-0';
-            break;
-        case 4:
-            classes += ' offset-md-0 offset-lg-2 offset-xl-0';
-            break;
-        case 5:
-            classes += ' offset-md-2 offset-lg-0 offset-xl-2';
-            break;
-        case 6:
-            classes += ' offset-md-0 offset-lg-0 offset-xl-0';
-            break;
-        case 7:
-            classes += ' offset-md-2 offset-lg-2 offset-xl-0';
-            break;
-        case 8:
-            classes += ' offset-md-0 offset-lg-0 offset-xl-0';
-            break;
-        case 9:
-            classes += ' offset-md-2 offset-lg-0 offset-xl-2';
-            break;
-        case 10:
-            classes += ' offset-md-0 offset-lg-2 offset-xl-0 col-xl-2';
-            break;
-        case 11:
-            classes += ' offset-md-2 offset-lg-0 col-xl-0';
-            break;
-        case 12:
-            classes += ' offset-md-0 offset-lg-0 offset-xl-0';
-            break;
-        default:
-            break;
-    }
     let linkLabel = props.type === 'learning' ? 'Show certificate' : 'Website';
     return(
-        <div className={classes}>
+        <div className="card">
             <div className="card-body">
             <h5 className="card-title">{props.title}</h5>
             <h6 className="card-subtitle mb-2 text-muted"><time>{props.year}</time></h6>
