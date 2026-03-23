aa60e63d0fe375fd2bd98840e7d69aad8102fce7
Adrian Szlegel <adrian95999@gmail.com>
2026-03-23 22:09:25 +0100

    info added

--- src/components/WelcomeSection.js
+++ src/components/WelcomeSection.js
@@ -7,6 +7,7 @@ function WelcomeSection() {
         <div id="welcome-section">
             <h1 id="name" className="text-center">Adrian Szlegel</h1>
             <p className="text-center">Full Stack Developer</p>
+            <h2 className='text-center'>This version of my portfolio website is obsolete. Check out my new design in the link below. In the meantime I'll work to change this React version ASAP.</h2>
             <p className="text-center" ><a href="https://adrszl.github.io/">Static version</a></p>
             <div className="text-center">
             <img src={logo} className="App-logo" alt="react" />
