42687912308c85744e3721508a22020d6d154778
Adrian Szlegel <adrian95999@gmail.com>
2026-03-24 23:52:14 +0100

    naming changed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import Header from './components/Header';
-import WelcomeSection from './components/WelcomeSection';
-import SliderSection from './containers/SliderSection';
-import AboutSection from './containers/AboutSection';
-import ContactSection from './containers/ContactSection';
-import Footer from './components/Footer';
+import Header from './old-components/Header';
+import WelcomeSection from './old-components/WelcomeSection';
+import SliderSection from './old-containers/SliderSection';
+import AboutSection from './old-containers/AboutSection';
+import ContactSection from './old-containers/ContactSection';
+import Footer from './old-components/Footer';
 import './App.css';
 
 function App() {
