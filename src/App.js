44b4c1d19bd11876b1c67ec8aafd1bfd0b1c3a67
Adrian Szlegel <adrian95999@gmail.com>
2026-03-25 12:25:18 +0100

    new css commented for now

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import AboutSection from './old-containers/AboutSection';
 import ContactSection from './old-containers/ContactSection';
 import Footer from './old-components/Footer';
 import './App.css';
-import './global.css';
+// import './global.css';
 import LoadingScreen from './components/loadingScreen';
 
 function App() {
