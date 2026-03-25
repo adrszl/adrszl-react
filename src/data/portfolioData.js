e8f146164f990402cd973d917daa3ca1d4dad4f9
Adrian Szlegel <adrian95999@gmail.com>
2026-03-25 12:22:55 +0100

    data added

---
+++ src/data/portfolioData.js
@@ -0,0 +1,131 @@
+// Portfolio data for carousel
+export const portfolioData = [
+    {
+        id: 1,
+        title: 'Website - 2019',
+        description: 'Personal website for Mr. Arkadiusz Trajdos',
+        image: 'img/project10.png',
+        tech: ['HTML', 'CSS', 'JS', 'PHP', 'Wordpress'],
+        projectLink: '404'
+    },
+    {
+        id: 2,
+        title: 'E-Commerce Website - 2020',
+        description: 'E-commerce website for PC Belfast',
+        image: 'img/project11.png',
+        tech: ['HTML', 'CSS', 'JS', 'PHP', 'Wordpress', 'WooCommerce'],
+        projectLink: 'https://www.pcbelfast.co.uk/'
+    },
+    {
+        id: 3,
+        title: 'Website - 2024',
+        description: 'Website built PIN Kultury with podcast player',
+        image: 'img/project16.png',
+        tech: ['HTML', 'CSS', 'JS', 'PHP', 'Wordpress'],
+        projectLink: '404'
+    },
+    {
+        id: 4,
+        title: 'Website - 2023',
+        description: 'Website built for Narimane and Karol, who were getting married',
+        image: 'img/project15.png',
+        tech: ['HTML', 'CSS', 'JS'],
+        projectLink: '404'
+    },
+    {
+        id: 5,
+        title: 'Website - 2023',
+        description: 'Website built for Compensum banking law office',
+        image: 'img/project14.png',
+        tech: ['HTML', 'CSS', 'JS', 'PHP', 'Wordpress'],
+        projectLink: '404'
+    },
+    {
+        id: 6,
+        title: 'E-Commerce Website - 2020',
+        description: 'E-commerce functionality for Mr. Arkadiusz Trajdos',
+        image: 'img/project12.png',
+        tech: ['HTML', 'CSS', 'JS', 'PHP', 'Wordpress', 'WooCommerce'],
+        projectLink: '404'
+    },
+    {
+        id: 7,
+        title: 'Magna Polonia Webpage - 2018 -> now',
+        description: 'Website for Magna Polonia academic corporation',
+        image: 'img/project13.png',
+        tech: ['HTML', 'CSS', 'JS', 'PHP', 'Wordpress'],
+        projectLink: 'https://magnapolonia.pl/'
+    },
+    {
+        id: 8,
+        title: 'Personal Portfolio Webpage - 2018',
+        description: 'Simple page intended to be "Web business card"',
+        image: 'img/project1.png',
+        tech: ['HTML', 'CSS'],
+        projectLink: 'https://codepen.io/adrszl/full/WaXoWw'
+    },
+    {
+        id: 9,
+        title: 'Tribute Page (Simple Landing Page) - 2018',
+        description: 'Simple page intended to be landing page with static content',
+        image: 'img/project2.png',
+        tech: ['HTML', 'CSS'],
+        projectLink: 'https://codepen.io/adrszl/full/EdjRPe/'
+    },
+    {
+        id: 10,
+        title: 'Survey Form - 2018',
+        description: 'Sample implementation of survey of some kind with some author\'s stylization',
+        image: 'img/project3.png',
+        tech: ['HTML', 'CSS'],
+        projectLink: 'https://codepen.io/adrszl/full/KGpEKx/'
+    },
+    {
+        id: 11,
+        title: 'Product Landing Page - 2018',
+        description: 'Sample implementation of landing page for example for a product',
+        image: 'img/project4.png',
+        tech: ['HTML', 'CSS'],
+        projectLink: 'https://codepen.io/adrszl/full/YJqrPv/'
+    },
+    {
+        id: 12,
+        title: 'Technical Documentation Page - 2018',
+        description: 'Sample implementation of technical documentation page',
+        image: 'img/project5.png',
+        tech: ['HTML', 'CSS'],
+        projectLink: 'https://codepen.io/adrszl/full/EdjRPe/'
+    },
+    {
+        id: 13,
+        title: 'Roman Numeral Converter - 2019',
+        description: 'Converter which converts number from decimal to roman and vice versa',
+        image: 'img/project6.png',
+        tech: ['JS'],
+        projectLink: 'https://codepen.io/adrszl/full/rPxRqB'
+    },
+    {
+        id: 14,
+        title: 'Palindrome Checker - 2019',
+        description: 'Checks if given phrase is a palindrome',
+        image: 'img/project7.png',
+        tech: ['JS'],
+        projectLink: 'https://codepen.io/adrszl/full/QYyYZM'
+    },
+    {
+        id: 15,
+        title: 'US Telephone Number Validator - 2019',
+        description: 'Validator checks if given number is valid US one',
+        image: 'img/project8.png',
+        tech: ['JS'],
+        projectLink: 'https://codepen.io/adrszl/full/JxXrWB'
+    },
+    {
+        id: 16,
+        title: 'Caesars Cipher - 2019',
+        description: 'Encrypts and decrypts given phrase with Caesars Cipher',
+        image: 'img/project9.png',
+        tech: ['JS'],
+        projectLink: 'https://codepen.io/adrszl/full/wNGqKE'
+    },
+];
