f9a185a4c025bb7d8b1c1e809bd70cc99d0b863e
Adrian Szlegel <adrian95999@gmail.com>
2026-03-25 12:23:28 +0100

    welcome loading screen set to 1500ms

--- src/components/loadingScreen.jsx
+++ src/components/loadingScreen.jsx
@@ -1,16 +1,28 @@
+import { useEffect, useState } from "react";
+
 const LoadingScreen = () => {
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setIsLoading(false);
+        }, 1500);
+
+        return () => clearTimeout(timer);
+    }, []);
+
     return (
-        <div className="loader" id="loader">
+        isLoading === true ? <div className="loader" id="loader">
             <div className="loader-content">
                 <div className="loader-prism">
                     <div className="prism-face"></div>
                     <div className="prism-face"></div>
                     <div className="prism-face"></div>
                 </div>
-                <div style={{color: 'var(--accent-blue)', fontSize: '18px', textTransform: 'uppercase', letterSpacing: '3px'}}>
+                <div style={{ color: 'var(--accent-blue)', fontSize: '18px', textTransform: 'uppercase', letterSpacing: '3px' }}>
                     Loading...</div>
             </div>
-        </div>
+        </div> : null
     );
 }
 
