de642ebf42f178744f97ae577a49af170afff089
Adrian Szlegel <adrian95999@gmail.com>
2026-03-27 12:30:59 +0100

    parallax added to the hero section

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -1,7 +1,27 @@
+import { useRef, useEffect } from "react";
+
 const HeroSection = () => {
+    const heroRef = useRef(null);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const scrolled = window.pageYOffset;
+
+            if (heroRef.current) {
+                heroRef.current.style.transform = `translateY(${scrolled * 0.7}px)`;
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
     return (
-        <section className="hero" id="home" style={{flexDirection: 'column'}}>
-            <h1 style={{fontSize: '56px'}}>Adrian Szlegel</h1>
+        <section className="hero" id="home" style={{ flexDirection: 'column' }} ref={heroRef}>
+            <h1 style={{ fontSize: '56px' }}>Adrian Szlegel</h1>
             <h2>Senior Full Stack Developer</h2>
         </section>
     );
