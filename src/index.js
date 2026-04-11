e107835f6c5ca2650e0f98c13e00f75d908a05f9
Adrian Szlegel <adrian95999@gmail.com>
2026-04-11 20:45:27 +0200

    bootstrap for older version of the app

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import ReactDOM from 'react-dom/client';
-// import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
